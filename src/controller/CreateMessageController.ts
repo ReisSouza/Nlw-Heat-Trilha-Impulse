152d5562a46fb8e3e88bdc894ac5806db34c29e6
ReisSouza <jacktrs@outlook.com>
2021-10-20 10:50:15 -0300

    Controle Message

--- src/controller/CreateMessageController.ts
+++ src/controller/CreateMessageController.ts
@@ -1,8 +1,14 @@
 import { Response, Request } from "express";
 
+import { CreateServiceMenssage } from "../services/CreateServiceMenssage";
+
 class CreateMessageController {
   async handle(request: Request, response: Response) {
-    const { text } = request.body;
+    const { message } = request.body;
+    const { user_id } = request;
+    const service = new CreateServiceMenssage();
+    const result = await service.execute(message, user_id);
+    return response.json(result);
   }
 }
 
